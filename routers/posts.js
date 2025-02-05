7371ba6f262a128c5674756348e9a75d3082cef3
penso996 <penso.andrea@outlook.it>
2025-02-05 15:18:34 +0100

    working on posts.js

--- routers/posts.js
+++ routers/posts.js
@@ -2,4 +2,39 @@
 const express = require("express");
 const router = express.Router();
 
+
+// Rotte di CRUD dei post
+
+// Index
+router.get("/", function (req, res) {
+    res.send("Lista dei post");
+});
+
+// Show
+router.get("/:id", function (req, res) {
+    res.send("Dettagli del post " + req.params.id);
+});
+
+// Store
+router.post("/", function (req, res) {
+    res.send("Creazione di un post");
+});
+
+// Update
+router.put("/:id", function (req, res) {
+    res.send("Modifica totale del post " + req.params.id);
+});
+
+// Modify
+router.patch("/:id", function (req, res) {
+    res.send("Modifica parziale del post " + req.params.id);
+});
+
+// Destroy
+router.delete('/:id', function (req, res) {
+    res.send("Eliminazione del post " + req.params.id);
+});
+
+
+// Esportiamo il modulo del router
 module.exports = router;
